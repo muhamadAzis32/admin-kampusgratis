4bdc14dfff895a7642ce252e996f7eb8139e7a3e
Muhamad Azis <88225954+muhamadAzis32@users.noreply.github.com>
2022-11-02 16:44:10 +0700

    fix bug css

--- resources/views/_partial/_header.blade.php
+++ resources/views/_partial/_header.blade.php
@@ -1,17 +1,17 @@
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<link rel="apple-touch-icon" sizes="76x76" href="{{ asset('assets/img/kgLogo.svg') }}">
-<link rel="icon" type="image/png" href="{{ asset('assets/img/KGLogo4.png') }}">
+<link rel="apple-touch-icon" sizes="76x76" href="{{ secure_asset('assets/img/kgLogo.svg') }}">
+<link rel="icon" type="image/png" href="{{ secure_asset('assets/img/KGLogo4.png') }}">
 
 <!--     Fonts and icons     -->
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
 
 <!-- Nucleo Icons -->
-<link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+<link href="{{ secure_asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
 
 <!-- Font Awesome Icons -->
 <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-<link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+<link href="{{ secure_asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
 
 <!-- CSS Files -->
-<link id="pagestyle" href="{{ asset('assets/css/soft-ui-dashboard.css?v=1.0.7') }}" rel="stylesheet" />
+<link id="pagestyle" href="{{ secure_asset('assets/css/soft-ui-dashboard.css?v=1.0.7') }}" rel="stylesheet" />
